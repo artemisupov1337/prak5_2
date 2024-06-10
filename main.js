d46dfdfa63fbdc9dfd39d49374575b012abccb0e
Artem Isupov <isupov.artom2006@gmail.com>
2024-06-10 13:09:15 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+let a1 = document.getElementById("a1")
+let a2 = document.getElementById("a2")
+let a3 = document.getElementById("a3")
+let a4 = document.getElementById("a4")
+let a5 = document.getElementById("a5")
+
+a1.addEventListener("click", (event) => {
+  alert("Вітчизна — ось і альфа, і омега!")
+});
+
+a2.addEventListener("click", (event) => {
+  alert("Нації вмирають не від інфаркту. Спочатку їм відбирає мову.")
+});
+
+a3.addEventListener("click", (event) => {
+  alert("Нема на світі України, немає другого Дніпра")
+});
+
+a4.addEventListener("click", (event) => {
+  alert("Ще не вмерла України і слава, і воля.")
+});
+
+a5.addEventListener("click", (event) => {
+  alert("Я дуже люблю народну українсь-ку мову, звучну, барвисту й таку м'я-ку.")
+});
